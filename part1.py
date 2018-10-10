8cf3b422516538a2337090e9ff683eba739d5252
Ahmer Ali <17100033@lums.edu.pk>
2018-10-11 02:50:38 +0500

    Fixing mistake

--- part1.py
+++ part1.py
@@ -2,9 +2,9 @@ import os
 import sys
 
 
-
-if os.path.exists(str(sys.argv[1])): #Checking for Valid Path
-	dirList = os.listdir(str(sys.argv[1]))
+directory = str(sys.argv[1])
+if os.path.exists(directory): #Checking for Valid Path
+	dirList = os.listdir(directory)
 	print dirList
 
 	topicsDictionary = {} 
@@ -26,11 +26,11 @@ if os.path.exists(str(sys.argv[1])): #Checking for Valid Path
 
 			#Maintaining a dictionary of number of files in each topic. Need to increment everytime when new Lecture is found.
 			if key in topicsDictionary.keys():
-				temp = 'downloads/'+courseName+'/'+ str(topicsDictionary[key])+ " - " +topic
+				temp =  directory + '/'+courseName+'/'+ str(topicsDictionary[key])+ " - " +topic
 				topicsDictionary[key] = topicsDictionary[key] + 1
 
 			else:
-				temp = 'downloads/'+courseName+'/ 1 - ' +topic
+				temp = directory + '/'+courseName+'/ 1 - ' +topic
 				topicsDictionary[key] = 1
 
 			
@@ -41,11 +41,11 @@ if os.path.exists(str(sys.argv[1])): #Checking for Valid Path
 				os.makedirs(temp)
 
 				#Renaming the Directory as now a new File has been added. So the Count goes up.
-			os.rename(temp,'downloads/'+courseName+'/'+ str(topicsDictionary[key])+ " - " +topic)	
+			os.rename(temp,directory + '/'+courseName+'/'+ str(topicsDictionary[key])+ " - " +topic)	
 
 			#Moving the File to its folder
-			source = 'downloads/'+i
-			dest = 'downloads/'+courseName+'/'+ str(topicsDictionary[key])+ " - " +topic+ '/' +lecture
+			source = directory + '/'+i
+			dest = directory + '/'+courseName+'/'+ str(topicsDictionary[key])+ " - " +topic+ '/' +lecture
 
 			
 			os.rename(source,dest)
